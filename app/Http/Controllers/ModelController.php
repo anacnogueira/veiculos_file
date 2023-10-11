1f075ccb5225131b5c2e2deca8d876d4c2e73414
Ana Claudia <anacnogueira@gmail.com>
2023-10-11 13:30:23 -0300

    Retorna um erro em caso de não encontrar a marca

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -10,6 +10,12 @@ class ModelController extends Controller
     public function index($brandId)
     {
         $models = FipeCarros::getModelos($brandId);
+
+        if (!$models) {
+            return response()->json([
+                'error' => 'Não foi possível obter os modelos da marca.'
+            ], 400);
+        }
         
         return $models['modelos'];
     }
